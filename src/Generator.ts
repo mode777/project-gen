ce988febdad9fc5d4143cd64fba0c17379297978
mode777 <mode7@gmx.net>
2017-05-24 00:08:53 +0200

    filesystem based replacement integrated

--- src/Generator.ts
+++ src/Generator.ts
@@ -3,6 +3,7 @@ import * as glob from 'glob';
 import { IFile } from './interfaces';
 import { BinaryFile } from './BinaryFile';
 import * as path from 'path';
+import * as fs from 'fs';
 
 export class Generator {
 
@@ -17,6 +18,7 @@ export class Generator {
         let collection = {};
         this._config.include.forEach(pattern =>
             glob.sync(path.join(this._config.inputDir, pattern))
+                .filter(file => !fs.lstatSync(file).isDirectory())
                 .forEach(file => collection[file] = true));
         
         this._files = Object.keys(collection)
